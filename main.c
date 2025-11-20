769e227222e22ceb9cb2e359e2fe8204221147b1
Dev-Moynul <stu.moynulislam@gmail.com>
2025-11-20 16:37:34 +0600

    palindrome number

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,original,rev=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    original=n;
+    while(n>0)
+    {
+        rev=rev*10+n%10;
+        n=n/10;
+    }
+    if(rev==original)
+        printf("%d is Pelindrome",original);
+    else
+        printf("%d is Not palindrome",original);
+    return 0;
+}
